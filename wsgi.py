050f32f46109814f972d46b894c736c32467cbc2
Daniele.Speziale <dspeziale@gmail.com>
2026-03-11 18:11:05 +0100

    Auto-initialize production database on Vercel boot

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,9 @@
 import os
-from app import create_app
+from app import create_app, db
+from app.utils.seed import seed_admin
 
 app = create_app(os.environ.get('FLASK_ENV', 'production'))
+
+with app.app_context():
+    db.create_all()
+    seed_admin()
